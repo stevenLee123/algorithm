f9ce731e5230f25248b9b49f09e4d63afe42887e
stevenLee <1144873128@qq.com>
2019-07-13 23:21:05 +0800

    LCS算法

--- src/com/steven/LCS/LCSDemo2.java
+++ src/com/steven/LCS/LCSDemo2.java
@@ -8,8 +8,6 @@
 package com.steven.LCS;
 
 
-
-
 public class LCSDemo2 {
 
     public static void main(String[] args) {
@@ -20,9 +18,60 @@ public class LCSDemo2 {
         // 输出2个最长对称字符串：pop/upu
         String input3 = "pop-upu";
         getLCS(input3);
+        String[] list = new String[]{"cool", "lock", "cook"};
+        System.out.println(getLongestSubStr(list));
+        int[] mmm = new int[]{1,2,34,56,77,99};
+        System.out.println(binarySearch(mmm,98));
 
     }
 
+    //查找最大公共串
+    public static char[] getLongestSubStr(String[] list) {
+        StringBuilder result = new StringBuilder();
+        int listLen = list.length;
+        int firstStrLen = list[0].length();
+        for (int i = 0; i < firstStrLen; i++) {
+            String currSubStr = list[0].substring(i, i + 1);
+            boolean currFlag = true;
+            String tmpSubStr = null;
+            for (int j = 0; j < listLen; j++) {
+                if (i > list[j].length()) {
+                    break;
+                } else {
+                    tmpSubStr = list[j].substring(i);
+                }
+                if (!tmpSubStr.contains(currSubStr)) {
+                    currFlag = false;
+                    break;
+                }
+            }
+            if (currFlag) {
+                result.append(currSubStr);
+            }
+
+        }
+        return result.toString().toCharArray();
+    }
+
+    public static int binarySearch(int[] nums, int value) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (value == nums[mid]) {
+                return mid;
+            }
+            if (value > nums[mid]) {
+                low = mid + 1;
+            }
+            if (value < nums[mid]) {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+
+    }
 
     public static int getLCS(String str) {
         StringBuilder sb = new StringBuilder(str);
@@ -56,37 +105,37 @@ public class LCSDemo2 {
     public static String getLCSSubString(int[][] LCSMatrix, char[] chars, int horIndex, int verIndex, StringBuilder result) {
 
         //横向找
-        for (int i = horIndex,j = verIndex; j >0; j--) {
+        for (int i = horIndex, j = verIndex; j > 0; j--) {
             // 如果是0，表示迭代结束
-            if(i == 0 || j == 0){
+            if (i == 0 || j == 0) {
                 break;
             }
-            if(LCSMatrix[i][j] == LCSMatrix[i][j-1]){
+            if (LCSMatrix[i][j] == LCSMatrix[i][j - 1]) {
                 continue;
-            }else{
+            } else {
                 result.append(chars[j - 1]);
                 i--;
             }
         }
         StringBuilder verResult = new StringBuilder();
         //纵向找
-        for (int i = horIndex,j = verIndex; i >0; i--) {
+        for (int i = horIndex, j = verIndex; i > 0; i--) {
             // 如果是0，表示迭代结束
-            if(i == 0 || j == 0){
+            if (i == 0 || j == 0) {
                 break;
             }
-            if(LCSMatrix[i][j] == LCSMatrix[i-1][j]){
+            if (LCSMatrix[i][j] == LCSMatrix[i - 1][j]) {
                 continue;
-            }else{
-                if(i == horIndex){
+            } else {
+                if (i == horIndex) {
                     break;
                 }
                 verResult.append(chars[j - 1]);
-                    j--;
+                j--;
             }
         }
-        if(verResult.length()>0){
-            result.append("/"+verResult);
+        if (verResult.length() > 0) {
+            result.append("/" + verResult);
         }
 
 
